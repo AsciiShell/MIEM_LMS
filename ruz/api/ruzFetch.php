ad8fa671193cfd7bc28de743f079953c6453f87d
asciishell <asciishell@yandex.ru>
2019-03-25 14:37:54 +0300

    Moodle update

--- ruz/api/ruzFetch.php
+++ ruz/api/ruzFetch.php
@@ -1,4 +1,7 @@
 <?php
 require_once("../lib.php");
+if (isCommandLineInterface()) {
+    define('CLI_SCRIPT', true);
+}
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode(DataBaseCourse::getInstance()->rusFetcher(), JSON_UNESCAPED_UNICODE);
